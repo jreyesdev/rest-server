0f4a0ddc167a1c5c3449c0c1eb8c10dd488b0478
jreyesdev <djrm1204@gmail.com>
2021-09-09 23:12:37 -0400

    Clase para la BD

---
+++ database/config.js
@@ -0,0 +1,33 @@
+const mongose = require('mongose')
+
+class DBMongo{
+    constructor(){
+        this.name = process.env.DB_NAME
+        this.user = process.env.DB_USERNAME
+        this.pass = process.env.DB_PASS
+        this.cluster = process.env.DB_CLUSTER
+    }
+
+    get urlCluster(){
+        return `mongodb+srv://${this.user}:${this.pass}@${this.cluster}/${this.name}?retryWrites=true&w=majority`
+    }
+
+    async connection(){
+        try{
+            await mongose.connect(this.urlCluster(),{
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useFindAndModify: false
+            })
+
+            console.log('DB is connected')
+            
+        }catch(err){
+            console.error(err)
+            throw new Error(err)
+        }
+    }
+}
+
+module.exports = new DBMongo()
