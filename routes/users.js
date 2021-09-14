40d648299a2c97086ec184301501b72ec75d579c
jreyesdev <djrm1204@gmail.com>
2021-09-14 19:11:59 -0400

    RUta POST con middleware

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const { Router } = require('express')
 const router = Router()
+const { check } = require('express-validator')
 
 const { getUsers, postUser, getUserById } = require('../controllers/userController')
+const { formRegister } = require('../middlewares/userRegister')
+const { isValidRol, emailExists } = require('../helpers/dbValidator')
 
 router.get('',getUsers)
 
 router.get('/:id',getUserById)
 
-router.post('',postUser)
+router.post('',[
+    check('name','El nombre es requerido').not().isEmpty(),
+    check('email').custom(emailExists),
+    check('password','La contraseña debe ser mayor o igual a 6 caracteres').isLength({min: 6}),
+    //check('role','No es un rol válido').isIn(['SUPER_ADMIN','ADMIN','USER']),
+    check('role').custom(isValidRol),
+    formRegister
+],postUser)
 
 module.exports = router
