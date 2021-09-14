573a1eaf775a6f1ca5214cd9e4616be084d0c297
jreyesdev <djrm1204@gmail.com>
2021-09-14 19:11:32 -0400

    Funcion para validar email

--- helpers/dbValidator.js
+++ helpers/dbValidator.js
@@ -1,12 +1,21 @@
 const Rol = require('../models/rol')
+const Usuario = require('../models/user')
 
-const isValidRol = async (rol='')=>{
-    const rol = await Rol.findOne({ rol })
-    if(!rol){
-        throw new Error(`El rol ${rol} no existe`)
+const isValidRol = async (rol='') => {
+    const role = await Rol.findOne({ rol })
+    if(!role){
+        throw new Error(`El rol ${role} no existe`)
+    }
+}
+
+const emailExists = async (email = '') => {
+    const correo = await Usuario.findOne({ email })
+    if(!correo){
+        throw new Error(`El correo ${correo} ya existe`)
     }
 }
 
 module.exports = {
-    isValidRol
+    isValidRol,
+    emailExists
 }
