e586a721dbe2b91fefbf893f9481ca29b47f9546
jreyesdev <djrm1204@gmail.com>
2021-09-15 22:48:23 -0400

    Middleware cambiado a validar campos

--- middlewares/fieldsValidate.js
+++ middlewares/fieldsValidate.js
@@ -1,6 +1,6 @@
 const { validationResult } = require('express-validator')
 
-const formRegister = (req, res, next) => {
+const validarCampos = (req, res, next) => {
     const errors = validationResult(req)
 
     if(!errors.isEmpty()) return res.status(400).json(errors)
@@ -9,5 +9,5 @@ const formRegister = (req, res, next) => {
 }
 
 module.exports = {
-    formRegister
+    validarCampos
 }
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const { check } = require('express-validator')
 const router = Router()
 
 const { getUsers, postUser, getUserById, putUser, deleteUser } = require('../controllers/userController')
-const { formRegister } = require('../middlewares/userRegister')
+const { validarCampos } = require('../middlewares/fieldsValidate')
 const { isValidRol, emailExists, existsIdUser } = require('../helpers/dbValidator')
 
 router.get('',getUsers)
@@ -11,7 +11,7 @@ router.get('',getUsers)
 router.get('/:id',[
     check('id').custom(existsIdUser),
     check('role').custom(isValidRol),
-    formRegister
+    validarCampos
 ],getUserById)
 
 router.post('',[
@@ -20,7 +20,7 @@ router.post('',[
     check('password','La contraseña debe ser mayor o igual a 6 caracteres').isLength({min: 6}),
     //check('role','No es un rol válido').isIn(['SUPER_ADMIN','ADMIN','USER']),
     check('role').custom(isValidRol),
-    formRegister
+    validarCampos
 ],postUser)
 
 router.put('/:id',putUser)
@@ -28,7 +28,7 @@ router.put('/:id',putUser)
 router.delete('/:id',[
     check('id','El ID no es válido').isMongoId(),
     check('id').custom(existsIdUser),
-    formRegister
+    validarCampos
 ],deleteUser)
 
 module.exports = router
