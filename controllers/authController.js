12a3f7ee1ef0b1555b03e78b0712e834b0c68294
jreyesdev <djrm1204@gmail.com>
2021-09-16 13:54:21 -0400

    inicio de session con google funcional

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs')
 const Controller = require('./controller')
 const Usuario = require('../models/user')
 const { generar } = require('../helpers/jwtFunctions')
+const { googleVerify } = require('../helpers/googleVerify')
 
 class AuthController extends Controller{
     constructor(){
@@ -26,6 +27,40 @@ class AuthController extends Controller{
             console.log(err)
             res.status(500).json({
                 mgs: "Error, contacte al administrador"
+            })
+        }
+    }
+
+    async googleSingIn(req = this.req, res = this.res){
+        const { id_token } = req.body
+        try {
+            const { email, name, image } = await googleVerify(id_token)
+            let usuario = await Usuario.findOne({ email })
+            if(!usuario){
+                usuario = new Usuario({
+                    name,
+                    email,
+                    password: '123456',
+                    image,
+                    google: true
+                })
+                await usuario.save()
+            }
+            if(usuario.delete){
+                return res.status(401).json({
+                    msg: 'Contacte al administrador'
+                })
+            }
+            const token = await generar(usuario.uid)            
+            res.json({
+                msg: 'Google singin successfully',
+                user: googleUser,
+                token
+            })
+        }catch(err){
+            console.log(err)
+            res.status(400).json({
+                msg: 'Token Google no válido'
             })
         }
     }
--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,17 @@ const { check } = require('express-validator')
 const router = Router()
 
 const { validarCampos } = require('../middlewares/fieldsValidate')
-const { login } = require('../controllers/authController')
+const { login, googleSingIn } = require('../controllers/authController')
 
 router.post('/login',[
     check('email','El email es obligatorio').isEmail(),
     check('password','La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ],login)
+
+router.post('/google',[
+    check('id_token','El id_token es necesario').not().isEmpty(),
+    validarCampos
+],googleSingIn)
 
 module.exports = router
