cd91dbd6272978cae9af52799ebad89bbf89fdfc
jreyesdev <djrm1204@gmail.com>
2021-09-16 01:29:30 -0400

    Funcion middleware para validar token

---
+++ middlewares/jwtValidate.js
@@ -0,0 +1,34 @@
+const { validar } = require('../helpers/jwtFunctions')
+const Usuario = require('../models/user')
+
+const validateJWT = (req, res, next) => {
+    const token = req.header('x-token')
+    if(!token){
+        return res.status(401).json({
+            msg: 'No hay token en la petición'
+        })
+    }
+    try{
+        const { uid } = validar(token)
+        const usuario = await Usuario.find({
+            _id: uid,
+            delete: false
+        })
+        if(!usuario){
+            return res.status(401).json({
+                msg: 'Token no existe'
+            })
+        }
+        req.usuario = usuario
+        next()
+    }catch(err) {
+        console.log(err)
+        res.status(400).json({
+            msg: 'Token no válido'
+        })
+    }
+}
+
+module.exports = {
+    validateJWT
+}
