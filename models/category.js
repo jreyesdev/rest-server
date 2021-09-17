0c62802944a1d56d671a50fa9979822a601ec790
jreyesdev <djrm1204@gmail.com>
2021-09-16 21:00:47 -0400

    Modelo de categoria

---
+++ models/category.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose')
+
+const CategorySchema = Schema({
+    name: {
+        type: String,
+        required: [true,'Name is required'],
+        unique: true
+    },
+    created:{
+        type: Date,
+        default: Date.now
+    },
+    created_by:{
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: [true,'Created_by is required']
+    },
+    updated:{
+        type: Date,
+        default: null
+    },
+    updated_by:{
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        default: null
+    },
+    delete:{
+        type: Date,
+        default: null
+    }
+})
+
+CategorySchema.methods.toJSON = function(){
+    const { __v, _id, ...categoria } = this.toObject()
+    categoria.uid = _id
+    return categoria
+}
+
+module.exports = model('Categoria',CategorySchema)
