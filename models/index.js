793e022855a5cc61fc49ac8d98c5f22dfbf43cd4
jreyesdev <djrm1204@gmail.com>
2021-09-16 21:05:19 -0400

    modelo principal para exportar todos

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Categoria = require('./category')
+const Role = require('./rol')
+const Server = require('./server')
+const Usuario = require('./user')
+
+module.exports = {
+    Categoria,
+    Role,
+    Server,
+    Usuario
+}
