901461d0d02c9565986e5fb8dfd78e4f343fc262
jreyesdev <djrm1204@gmail.com>
2021-10-06 21:36:40 -0400

    Ruta search

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server{
     constructor(){
         this.app = express()
         this.port = process.env.APP_PORT
-        // this.dbconnect()
+        this.dbconnect()
         this.middlewares()
         this.routes()
     }
@@ -25,6 +25,7 @@ class Server{
         this.app.use('/api/auth',require('../routes/auth'))
         this.app.use('/api/category',require('../routes/categories'))
         this.app.use('/api/products',require('../routes/products'))
+        this.app.use('/api/search',require('../routes/search'))
         this.app.use('/api/users',require('../routes/users'))
     }
 
