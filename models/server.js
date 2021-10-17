b583102b6c9dfaf9b84a284318eca0597e59842b
jreyesdev <djrm1204@gmail.com>
2021-10-17 15:58:22 -0400

    Ruta uploads

--- models/server.js
+++ models/server.js
@@ -26,6 +26,7 @@ class Server{
         this.app.use('/api/category',require('../routes/categories'))
         this.app.use('/api/products',require('../routes/products'))
         this.app.use('/api/search',require('../routes/search'))
+        this.app.use('/api/uploads',require('../routes/uploads'))
         this.app.use('/api/users',require('../routes/users'))
     }
 
