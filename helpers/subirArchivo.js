89e5716b74ce0d384d69ec2b1c2847895764bb47
jreyesdev <djrm1204@gmail.com>
2021-10-17 23:07:52 -0400

    Middleware para validar archivo en request

--- helpers/subirArchivo.js
+++ helpers/subirArchivo.js
@@ -2,11 +2,23 @@ const fs = require("fs")
 const path = require("path")
 const { v4: uuidv4 } = require("uuid")
 
+const UPLOADS_PATH = path.join(__dirname,'../uploads')
+
+const validarArchivo = (req, res, next) => {
+  if (
+    !req.files ||
+    Object.keys(req.files).length === 0 ||
+    !req.files.archivo
+  ) {
+    return res.status(400).json({ msg: "No files were uploaded." })
+  }
+  next()
+}
+
 const subirArchivo = async (file, extensiones, dir = "") => {
   return new Promise((res, rej) => {
     const { archivo } = file,
-      dirUpload = path.join(__dirname, "../uploads"),
-      dirFile = path.join(dirUpload, dir),
+      dirFile = path.join(UPLOADS_PATH, dir),
       nombreArr = archivo.name.split("."),
       ext = nombreArr[nombreArr.length - 1]
 
@@ -18,8 +30,8 @@ const subirArchivo = async (file, extensiones, dir = "") => {
     }
 
     // Si no existe carpeta UPLOADS la crea
-    if (!fs.existsSync(dirUpload)) {
-      fs.mkdirSync(dirUpload)
+    if (!fs.existsSync(UPLOADS_PATH)) {
+      fs.mkdirSync(UPLOADS_PATH)
     }
     // Si no existe carpeta UPLOADS/DIR la crea
     if (dir !== '' && !fs.existsSync(dirFile)) {
@@ -38,6 +50,11 @@ const subirArchivo = async (file, extensiones, dir = "") => {
   })
 }
 
+const deleteFile = async(file) => {
+  const pathFile = path.join(UPLOADS_PATH,file)
+  if(fs.existsSync(pathFile)) await fs.unlinkSync(pathFile)
+}
+
 const allowedCollec = async(col='') => {
   const collects = ['users','products']
   if(!collects.includes(col)){
@@ -46,6 +63,8 @@ const allowedCollec = async(col='') => {
 }
 
 module.exports = {
+  allowedCollec,
+  deleteFile,
   subirArchivo,
-  allowedCollec
+  validarArchivo
 }
--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -3,15 +3,22 @@ const { check } = require('express-validator')
 const { retornoMid } = require('./index')
 const { validateJWT } = require('./jwtValidate')
 
-const { allowedCollec } = require('../helpers/subirArchivo')
+const { allowedCollec, validarArchivo } = require('../helpers/subirArchivo')
 const { existsCollectId } = require('../helpers/dbValidator')
 
+const PostArchivo = retornoMid([
+    validarArchivo
+])
+
 const PutImage = retornoMid([
+    validateJWT,
+    validarArchivo,
     check('id','El id debe ser id de mongo').isMongoId(),
     check('col').custom(allowedCollec),
-    check('col').custom(existsCollectId)
+    check('col').custom(existsCollectId),
 ])
 
 module.exports = {
+    PostArchivo,
     PutImage
 }
