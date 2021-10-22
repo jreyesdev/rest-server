8ed5f92d0f39645812a78fc0eee97c9ff9cdbd67
jreyesdev <djrm1204@gmail.com>
2021-10-22 00:44:09 -0400

    comprobacion usuario conectado

--- controllers/sockets/socketController.js
+++ controllers/sockets/socketController.js
@@ -1,5 +1,9 @@
-const socketController = socket => {
-    console.log('Cliente conectado ' + socket.id)
+const { comprobarJWT } = require('../../middlewares/jwtValidate')
+
+const socketController = async(socket) => {
+    const user = await comprobarJWT(socket.handshake.headers['x-token'])
+    if(!user) socket.disconnect()
+    console.log('Cliente conectado ' + user.name + ' ' + socket.id)
 }
 
 module.exports = { socketController }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,34 @@
-const socket = io();
+const url = window.location.origin + "/api/auth/";
+let usuario = null;
 
-console.log('chat');
+const validaJWT = async () => {
+    const token = localStorage.getItem('token') || '';
+    if(token.length < 10){
+        window.location = 'index.html';
+        throw new Error('No hay token');
+    }
+    try {
+        const resp = await fetch(url,{ headers: { 'x-token': token } });
+        const { user, token: tok } = await resp.json();
+        localStorage.setItem('token',tok);
+        usuario = user;
+        document.title = 'Socket Chat - ' + user.name;
+        await conectarSocket();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const conectarSocket = async () => {
+    const socket = io({
+        'extraHeaders': {
+            'x-token': localStorage.getItem('token')
+        }
+    });
+}
+
+const main = async () => {
+    await validaJWT();
+}
+
+main();
