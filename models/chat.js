df01288df28a7c01bf0a590b97430c4575e1cf14
jreyesdev <djrm1204@gmail.com>
2021-10-22 01:37:56 -0400

    Modelo de chat

---
+++ models/chat.js
@@ -0,0 +1,31 @@
+class Mensaje {
+    constructor( uid, nombre, mensaje ) {
+        this.uid     = uid;
+        this.nombre  = nombre;
+        this.mensaje = mensaje;
+    }
+}
+
+class ChatMensajes{
+    constructor(){
+        this.mensajes = []
+        this.usuarios = {}
+    }
+    get ultimosMsg(){
+        return this.mensajes.splice(0,10)
+    }
+    get usuariosArr(){
+        return Object.values(this.usuarios)
+    }
+    enviarMensaje(uid, nombre, mensaje){
+        this.mensajes.unshift( new Mensaje(uid, nombre, mensaje))
+    }
+    conectarUsuario(usuario){
+        this.usuarios[usuario.id] = usuario
+    }
+    desconectarUsuario( id ){
+        delete this.usuarios[id]
+    }
+}
+
+module.exports = new ChatMensajes()
